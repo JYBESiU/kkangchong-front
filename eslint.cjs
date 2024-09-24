b1803faf01eb843bade797aadce8d0b96ac87120
JYBESiU <jychoi.besiu@gmail.com>
2024-09-24 15:55:24 +0900

    remove unused files, add dependencies

---
+++ eslint.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
