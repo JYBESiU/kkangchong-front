ca7273db00ae45b033eb2beaa67d791adde6c68c
JYBESiU <jychoi.besiu@gmail.com>
2024-09-24 16:03:29 +0900

    router 기본 설정

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from 'pages/Home';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
