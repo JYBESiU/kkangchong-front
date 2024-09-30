f65f86a9a91a955ad198a204b467f8ecf91504fd
JYBESiU <jychoi.besiu@gmail.com>
2024-09-30 21:15:06 +0900

    facility store 구현

--- src/store/useFacilityStore.ts
+++ src/store/useFacilityStore.ts
@@ -1,9 +1,16 @@
+import { Facility } from 'types';
 import { create } from 'zustand';
 
 interface FacilityState {
-  facilities: any[];
+  facilities: Facility[];
+  selectedFacility: Facility | null;
 }
 
-const useFacilityStore = create<FacilityState>()((set) => ({
+export const useFacilityStore = create<FacilityState>()((set) => ({
   facilities: [],
+  selectedFacility: null,
+
+  setFacilities: (data: Facility[]) => set(() => ({ facilities: data })),
+  setSelectedFacility: (data: Facility) =>
+    set(() => ({ selectedFacility: data })),
 }));
