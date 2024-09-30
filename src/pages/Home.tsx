48b44e35658e5f31b545a61559167da7941a1811
nel898 <seowonnoh9@gamil.com>
2024-09-30 17:49:57 +0900

    kakaomap component 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import KakaoMap from 'components/KakaoMap';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <KakaoMap></KakaoMap>
+    </div>
+  );
 }
 
 export default Home;
