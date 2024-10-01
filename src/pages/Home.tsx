4f2e6e6bb48a19c795ea27531ca3e9b10e95a6de
ksiwon <pjo12346@naver.com>
2024-10-01 22:10:57 +0900

    master와 facilitydetail 합침

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,7 @@ import FacilityList from 'components/FacilityList';
 import KakaoMap from 'components/KakaoMap';
 
 function Home() {
-<<<<<<< HEAD
   return <div>Home</div>;
-=======
-  return <div></div>;
->>>>>>> 41d75da (facilitylist 수정 사항 적용)
 }
 
 export default Home;
