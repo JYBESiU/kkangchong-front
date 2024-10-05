130969af8f308647dcb8eab400d2d3ca43e8b120
nel898 <seowonnoh9@gamil.com>
2024-10-05 15:01:35 +0900

    FacilitySearch Page Implementation

---
+++ src/components/Facility/FacilitySearch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './StyledButton';
+
+const FacilitySearch: React.FC = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+};
+
+export default FacilitySearch;
