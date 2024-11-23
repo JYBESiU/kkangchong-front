fb869537742f2bb81e5a8c3edc4d5aaacfd138de
nel898 <seowonnoh9@gamil.com>
2024-11-24 07:41:29 +0900

    Added wrappers after implementing MeasurementContext

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,21 @@ import Home from 'pages/Home';
 import MeasuringPage from 'pages/MeasuringPage';
 import ClubsPage from 'pages/ClubsPage';
 import ResultPage from 'pages/ResultPage';
+import { MeasurementProvider } from 'components/MeasurementContext';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="measuring" element={<MeasuringPage />} />
+        <Route
+          path="measuring"
+          element={
+            <MeasurementProvider>
+              <MeasuringPage />
+            </MeasurementProvider>
+          }
+        />
         <Route path="clubs" element={<ClubsPage />} />
         <Route path="result" element={<ResultPage />} />
       </Routes>
