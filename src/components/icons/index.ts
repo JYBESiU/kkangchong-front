711083ea83f3fd30c657b432c1dec10db5036f1b
nel898 <seowonnoh9@gamil.com>
2024-11-26 03:28:25 +0900

    svg file index

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as ArmMeasureVector } from './ArmMeasureVector';
+export { default as MagnifyingGlassTiltedRight } from './MagnifyingGlassTiltedRight';
+export { default as MoveMeasureVector } from './MoveMeasureVector';
+export { default as PageWithCurl } from './PageWithCurl';
+export { default as RotateLeftVector } from './RotateLeftVector';
+export { default as RotateRightVector } from './RotateRightVector';
+export { default as TiltLeftVector } from './TiltLeftVector';
+export { default as TiltRightVector } from './TiltRightVector';
