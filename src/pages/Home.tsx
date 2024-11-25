3c360baf8488812e4c42f977593b03f26d13d97d
nel898 <seowonnoh9@gamil.com>
2024-11-26 03:24:03 +0900

    first page ui implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { Button, Text } from 'components/shared';
 import { colors } from 'utils/color';
+import { Icon } from 'components/IconContext';
 
 function Home() {
   const navigate = useNavigate();
@@ -13,16 +14,21 @@ function Home() {
 
   return (
     <Root>
+      {IconOneElement}
+      {IconTwoElement}
+      <div style={{ height: '297px' }} />
       <Text fontSize={24} fontWeight={700}>
         AI 맞춤 운동 추천
       </Text>
+
       <Text textAlign={'center'}>
         총 6단계로 카메라를 이용하여
         <br />
         사용자의 신체 능력을 측정하고
         <br />
-        맞춤 운동을 추천해줘요
+        맞춤 운동 동호회를 추천해줘요
       </Text>
+
       <Button
         width={'314px'}
         height={'60px'}
@@ -42,6 +48,34 @@ const Root = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  max-width: 400px;
+  width: 393px;
+  height: 852px;
   background-color: ${colors.blue1};
 `;
+
+const IconOneElement = (
+  <div
+    style={{
+      width: 88.7,
+      height: 63.9,
+      position: 'absolute',
+      left: 113,
+      top: 189,
+    }}
+  >
+    <Icon icon="PageWithCurl" />
+  </div>
+);
+const IconTwoElement = (
+  <div
+    style={{
+      width: 33.1,
+      height: 53.79,
+      position: 'absolute',
+      left: 77,
+      top: 246,
+    }}
+  >
+    <Icon icon="MagnifyingGlassTiltedRight" />
+  </div>
+);
