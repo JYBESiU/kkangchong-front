a0ac09941364acec4076f4b1e1438998bed8ecdf
nel898 <seowonnoh9@gamil.com>
2024-11-26 21:28:52 +0900

    stepper icon

---
+++ src/components/icons/Stepper4.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgStepper4 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={95}
+    height={10}
+    fill="none"
+    {...props}
+  >
+    <circle cx={5} cy={5} r={5} fill="#E9E9E9" />
+    <circle cx={22} cy={5} r={5} fill="#E9E9E9" />
+    <circle cx={39} cy={5} r={5} fill="#E9E9E9" />
+    <circle cx={56} cy={5} r={5} fill="#4A77EA" />
+    <circle cx={73} cy={5} r={5} fill="#E9E9E9" />
+    <circle cx={90} cy={5} r={5} fill="#E9E9E9" />
+  </svg>
+);
+export default SvgStepper4;
