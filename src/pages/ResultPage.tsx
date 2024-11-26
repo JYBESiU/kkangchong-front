9c8d09fa75e87523d41955f636a34765c04e4dbc
JYBESiU <jychoi.besiu@gmail.com>
2024-11-26 22:09:57 +0900

    delete dummy

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -76,8 +76,8 @@ function ResultPage({}: ResultPageProps) {
       ) : selectedSports ? (
         <RecommendSport
           sports={selectedSports}
-          score={getScore(recommendDummy, selectedSports)}
-          reason={getReason(recommendDummy, selectedSports)}
+          score={getScore(recommendResult, selectedSports)}
+          reason={getReason(recommendResult, selectedSports)}
           explanation={explanations[selectedSports]}
           youtubeLink={youtubeLinks[selectedSports]}
           handleBackClick={() => setSelectedSports(null)}
@@ -111,27 +111,6 @@ const Loading = styled.div`
   gap: 30px;
 `;
 
-const recommendDummy = {
-  basketball_score: 5,
-  basketball_reason:
-    '사용자의 팔을 양옆으로 드는 능력이 약간 제한적이지만, 팔 근력이 양호하고 코어 힘이 안정적입니다. 허리 회전이 균형적이나 각도가 조금 낮아 몸통의 회전 속도에 약간의 제한이 있을 수 있습니다.',
-  table_tennis_score: 4,
-  table_tennis_reason:
-    '몸통 회전 각도와 기울임이 적절하나, 빠른 회전 동작에서 다소 부족할 수 있습니다. 코어 힘이 비교적 안정적이어서 기본적인 탁구 동작 수행에는 문제가 없습니다.',
-  rugby_score: 3,
-  rugby_reason:
-    '강한 몸통 조절과 다방향 움직임이 필요한 럭비에 비해 허리 회전 각도가 낮고 코어 힘이 다소 부족합니다. 상체의 안정성은 어느 정도 유지되지만, 격한 움직임에서 어려움이 예상됩니다.',
-  fencing_score: 2,
-  fencing_reason:
-    '팔 근력은 적절하고 허리 회전도 가능하지만, 앉은 자세에서의 빠른 균형 조절 능력이 약간 부족할 수 있습니다. 방향 전환 시 안정성을 유지하는 데 약간의 제한이 있을 것입니다.',
-  dance_sports_score: 1,
-  dance_sports_reason:
-    '골반과 몸통의 움직임이 적절하며, 기본적인 코어 힘이 안정적입니다. 빠른 회전 동작에서 약간의 제한이 있을 수 있지만, 대부분의 동작을 수행하는 데 무리가 없습니다.',
-  athletics_score: 0,
-  athletics_reason:
-    '상지 기능과 코어 힘이 적절하여 휠체어 조작에 유리합니다. 직선 주행에서 안정성이 있으며, 몸통의 회전이 자유롭지는 않지만 기본적인 추진력 생성에 무리가 없습니다.',
-};
-
 const explanations = {
   [RecommendSports.ATHLETICS]:
     '휠체어 육상은 특수 설계된 레이싱 휠체어를 이용해 트랙이나 도로에서 스피드와 체력을 겨루는 스포츠입니다.',
@@ -156,36 +135,46 @@ const youtubeLinks = {
   [RecommendSports.TABLE_TENNIS]: 'd7CMGfsObBk',
 };
 
-const getScore = (recommend: RecommendResult, sports: RecommendSports) => {
+const getScore = (
+  recommend: RecommendResult | null,
+  sports: RecommendSports
+) => {
   switch (sports) {
+    case null:
+      return 0;
     case RecommendSports.ATHLETICS:
-      return recommend.athletics_score;
+      return recommend?.athletics_score;
     case RecommendSports.BASKETBALL:
-      return recommend.basketball_score;
+      return recommend?.basketball_score;
     case RecommendSports.DANCING_SPORTS:
-      return recommend.dance_sports_score;
+      return recommend?.dance_sports_score;
     case RecommendSports.FENCING:
-      return recommend.fencing_score;
+      return recommend?.fencing_score;
     case RecommendSports.RUGBY:
-      return recommend.rugby_score;
+      return recommend?.rugby_score;
     case RecommendSports.TABLE_TENNIS:
-      return recommend.table_tennis_score;
+      return recommend?.table_tennis_score;
   }
 };
 
-const getReason = (recommend: RecommendResult, sports: RecommendSports) => {
+const getReason = (
+  recommend: RecommendResult | null,
+  sports: RecommendSports
+) => {
   switch (sports) {
+    case null:
+      return '';
     case RecommendSports.ATHLETICS:
-      return recommend.athletics_reason;
+      return recommend?.athletics_reason;
     case RecommendSports.BASKETBALL:
-      return recommend.basketball_reason;
+      return recommend?.basketball_reason;
     case RecommendSports.DANCING_SPORTS:
-      return recommend.dance_sports_reason;
+      return recommend?.dance_sports_reason;
     case RecommendSports.FENCING:
-      return recommend.fencing_reason;
+      return recommend?.fencing_reason;
     case RecommendSports.RUGBY:
-      return recommend.rugby_reason;
+      return recommend?.rugby_reason;
     case RecommendSports.TABLE_TENNIS:
-      return recommend.table_tennis_reason;
+      return recommend?.table_tennis_reason;
   }
 };
